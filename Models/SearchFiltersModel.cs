daa4befdd693e8141820dfdd0d76804634893bb6
George Mikelis <giorgos.mikelis@gmail.com>
2020-03-03 19:31:35 +0200

    com

---
+++ Models/SearchFiltersModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotels.Models
+{
+    public class SearchFiltersModel
+    {
+        public string[] Cities { get; set; }
+        public Tuple<int, string>[] RoomTypes { get; set; }
+    }
+}
