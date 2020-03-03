337c4a73ae014fc703b8ae477a9dbad3b147589e
George Mikelis <giorgos.mikelis@gmail.com>
2020-03-03 16:28:56 +0200

    com

--- Controllers/RoomListController.cs
+++ Controllers/RoomListController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Hotels.Models;
 
 namespace Hotels.Controllers
 {
     public class RoomListController : Controller
     {
+        private WdaContext _context;
+        
+        public RoomListController(WdaContext context)
+        {
+            _context = context;
+           
+
+        }
         public IActionResult Index()
         {
             return View();
