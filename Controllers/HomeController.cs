da4292abc15ad41103b6789c33c1b084496daca1
George Mikelis <giorgos.mikelis@gmail.com>
2020-03-04 18:12:52 +0200

    com

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,8 +23,19 @@ namespace Hotels.Controllers
 
         public IActionResult Index()
         {
-            var roomTypes = _context.RoomType;
-            return View();
+            var allCities = _context.Room.Select(room => room.City).Distinct().ToArray();
+            var allRoomTypes = _context.RoomType
+                .Select(x => Tuple.Create(x.Id, x.RoomType1)).ToArray();
+            var allGuestsCounts = _context.Room.Select(room => room.CountOfGuests).Distinct().ToArray();
+
+            var model = new Hotels.Models.SearchFiltersModel
+            {
+                CountOfGuests = allGuestsCounts,
+                Cities = allCities,
+                RoomTypes = allRoomTypes
+            };
+
+            return View(model);
         }
 
         public IActionResult Privacy()
