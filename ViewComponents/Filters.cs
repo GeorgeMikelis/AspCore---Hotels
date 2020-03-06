ce6118019134cbe96de168bfa0005ccefda6e952
George Mikelis <giorgos.mikelis@gmail.com>
2020-03-06 19:46:59 +0200

    com

---
+++ ViewComponents/Filters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hotels.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotels.ViewComponents
+{
+    public class FiltersViewComponent : ViewComponent
+    {
+        private readonly WdaContext _context;
+        
+        public FiltersViewComponent(WdaContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var cities = _context.Room
+                .Select(i => i.City)
+                .Distinct();
+
+            return View(cities);
+        }
+    }
+}
