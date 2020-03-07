e3ef1c9fd93cdaae5710246ca25149f9e66e8a2c
George Mikelis <giorgos.mikelis@gmail.com>
2020-03-07 13:13:42 +0200

    com

--- ViewComponents/Filters.cs
+++ ViewComponents/Filters.cs
@@ -18,11 +18,20 @@ namespace Hotels.ViewComponents
 
         public IViewComponentResult Invoke()
         {
-            var cities = _context.Room
-                .Select(i => i.City)
-                .Distinct();
+            var allCities = _context.Room.Select(room => room.City).Distinct().ToArray();
+            var allRoomTypes = _context.RoomType
+                .Select(x => Tuple.Create(x.Id, x.RoomType1)).ToArray();
+            var allGuestsCounts = _context.Room.Select(room => room.CountOfGuests).Distinct().ToArray();
 
-            return View(cities);
+            var model = new Hotels.Models.SearchFiltersModel
+            {
+                CountOfGuests = allGuestsCounts,
+                Cities = allCities,
+                RoomTypes = allRoomTypes
+            };
+
+            return View(model);
         }
+
     }
 }
